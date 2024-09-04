941bbbcf3302ea7293142f651a7dbd8631f8abdd
sunniesfish <hhch5611.jh@gmail.com>
2024-09-04 21:10:49 +0900

    creating api

---
+++ src/main/java/com/sunniesfish/todo_app/todo/dto/BoardsDTO.java
@@ -0,0 +1,17 @@
+package com.sunniesfish.todo_app.todo.dto;
+
+import com.sunniesfish.todo_app.todo.entity.ToDo;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BoardsDTO {
+    private long boardId;
+    private String title;
+    private List<ToDo> toDoList;
+}
